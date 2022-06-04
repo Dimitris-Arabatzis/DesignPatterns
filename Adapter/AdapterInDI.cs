a8730c8d1d4c4369841b14fefa0710f51c287ed5
Dimitris Arabatzis <dimitris.arabatzis@cuberm.com>
2022-06-04 13:20:51 +0300

    Adapter in DI with autofac

---
+++ Adapter/AdapterInDI.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    public interface ICommand
+    {
+        public void Execute();
+    }
+
+    public class SaveCommand : ICommand
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Saving file!..");
+        }
+    }
+    public class OpenCommand : ICommand
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Opening file!..");
+        }
+    }
+
+    public class Button
+    {
+        private ICommand _command;
+        private string name;
+
+        public Button(ICommand command, string name)
+        {
+            _command = command;
+            this.name = name;
+        }
+
+        public void Click()
+        {
+            _command.Execute();
+        }
+
+        public void PrintMe()
+        {
+            Console.WriteLine($"I am a button called {name}");
+        }
+    }
+
+    public class Editor
+    {
+        IEnumerable<Button> buttons;
+        public IEnumerable<Button> Buttons => buttons;
+
+        public Editor(IEnumerable<Button> buttons)
+        {
+            this.buttons = buttons;
+        }
+
+        public void ClickAll()
+        {
+            foreach (var btn in buttons)
+                btn.Click();
+        }
+
+    }
+}
--- Adapter/Program.cs
+++ Adapter/Program.cs
@@ -1,4 +1,6 @@
-﻿using MoreLinq;
+﻿using Autofac;
+using Autofac.Features.Metadata;
+using MoreLinq;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -21,7 +23,26 @@ namespace Adapter
 
         static void Main(string[] args)
         {
+            //---------------------Simple Adapter---------------------
             Draw();
+            //---------------------Adapter in DI---------------------
+
+            var b = new ContainerBuilder();
+            b.RegisterType<SaveCommand>().As<ICommand>().WithMetadata("Name", "Save");
+            b.RegisterType<OpenCommand>().As<ICommand>().WithMetadata("Name", "Open");
+            //b.RegisterType<Button>();
+            //b.RegisterAdapter<ICommand, Button>(cmd => new Button(cmd));
+            b.RegisterAdapter<Meta<ICommand>, Button>(cmd => 
+                new Button(cmd.Value, (string)cmd.Metadata["Name"]));
+            b.RegisterType<Editor>();
+
+            using (var c = b.Build())
+            {
+                var editor = c.Resolve<Editor>();
+                //editor.ClickAll();
+                foreach (var btn in editor.Buttons)
+                    btn.PrintMe();
+            }
         }
 
         private static void Draw()
