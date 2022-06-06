5c28cda7125ae10eb9ae50fa745612ac9ebc1cfd
Dimitris Arabatzis <dimitris.arabatzis@cuberm.com>
2022-06-06 16:59:09 +0300

    Create Example of Neural Network with composite pattern

---
+++ Composite/NeuralNetworkWithComposite.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite
+{
+    public static class ExtensionMethods
+    {
+        public static void ConnectTo(this IEnumerable<Neuron> self, IEnumerable<Neuron> other)
+        {
+            if(ReferenceEquals(self, other)) return;
+
+            foreach(var from in self)  
+            foreach(var to in other)
+                {
+                    from.Out.Add(to);
+                    to.In.Add(from);
+                }  
+        }
+    }
+
+    public class Neuron : IEnumerable<Neuron>
+    {
+        public float Value;
+        public List<Neuron> In, Out;
+
+        public IEnumerator<Neuron> GetEnumerator()
+        {
+            yield return this;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    public class NeuronLayer : Collection<Neuron>
+    {
+
+    }
+}
--- Composite/Program.cs
+++ Composite/Program.cs
@@ -19,7 +19,19 @@
             drawing.Children.Add(group);
 
             Console.WriteLine(drawing);
-            
+
+            //---------------------Implementation of Composite in NeuralNetworks---------------------
+            var neuron1 = new Neuron();
+            var neuron2 = new Neuron();
+
+            neuron1.ConnectTo(neuron2);
+
+            var layer1 = new NeuronLayer();
+            var layer2 = new NeuronLayer();
+
+            neuron1.ConnectTo(layer1);
+            layer1.ConnectTo(layer2);
+
         }
     }
 }
