a1f32bdfbf213891501ba1bef261e89d69327a18
Dimitris Arabatzis <dimitris.arabatzis@cuberm.com>
2022-06-09 00:27:28 +0300

    Create simple implementation of decorator with a custom string builder

---
+++ Decorator/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Decorator
+{
+    internal class Program
+    {
+        /// <summary>
+        /// The purpose of the pattern is to add functionality to an object withoud altering the code or rewriting the class. (Open-closed principle).
+        /// It keeps new functionality separate (Single Resp. Principle)
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            //---------------------Custom String Builder---------------------
+            var cb = new CodeBuilder();
+            cb.AppendLine("class Foo")
+              .AppendLine("{")
+              .AppendLine("}");
+            Console.WriteLine(cb);
+            //---------------------Custom String Builder---------------------
+        }
+    }
+}
