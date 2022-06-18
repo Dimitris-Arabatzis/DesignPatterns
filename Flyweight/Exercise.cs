dbb150720d17622c6e80dc65345b8717cad7cfc3
Dimitris Arabatzis <dimitris.arabatzis@cuberm.com>
2022-06-18 13:46:37 +0300

    Implement the flyweight pattern with a sample and an exercise

---
+++ Flyweight/Exercise.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Coding.Exercise
+{
+    public class Sentence
+    {
+        private string[] words;
+        public Dictionary<int,WordToken> tokens = new Dictionary<int, WordToken>();
+
+        public Sentence(string plainText)
+        {
+            words = plainText.Split(' ');
+        }
+
+        public WordToken this[int index]
+        {
+            get
+            {
+                var token = new WordToken();
+                tokens.Add(index, token);
+                return token;
+            }
+        }
+
+        public override string ToString()
+        {
+            List<string> sentence = new List<string>();
+            for (int i = 0; i < words.Length; i++)
+            {
+                var word = words[i];
+
+                if(tokens.ContainsKey(i) && tokens[i].Capitalize)
+                    word = word.ToUpperInvariant();
+
+                sentence.Add(word);
+            }
+            return string.Join(" ",sentence);
+        }
+
+        public class WordToken
+        {
+            public bool Capitalize;
+        }
+    }
+
+    class Execute
+    {
+        static void Main(string[] args)
+        {
+            var sentence = new Sentence("hello world");
+            sentence[1].Capitalize = true;
+            Console.WriteLine(sentence); //writes hello WORLD
+        }
+    }
+    
+}
