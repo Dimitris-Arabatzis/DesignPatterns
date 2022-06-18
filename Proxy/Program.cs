870cbf0436a5a18923e554655569f50a3100c656
Dimitris Arabatzis <dimitris.arabatzis@cuberm.com>
2022-06-18 14:00:49 +0300

    Create project for the proxy pattern

---
+++ Proxy/Program.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Proxy
+{
+
+    /// <summary>
+    /// A class that functions as an interface to a particular resource. That resource may be remote, expensive to construct
+    /// ,may require logging or some other added functionality.
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
