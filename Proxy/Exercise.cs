e9d39af2e5f2a0a8f4304978569725883cc9c31c
Dimitris Arabatzis <dimitris.arabatzis@cuberm.com>
2022-06-27 00:03:48 +0300

    Exercise of Proxy design pattern

---
+++ Proxy/Exercise.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    using System;
+
+    namespace Coding.Exercise
+    {
+        public class Person
+        {
+            public int Age { get; set; }
+
+            public string Drink()
+            {
+                return "drinking";
+            }
+
+            public string Drive()
+            {
+                return "driving";
+            }
+
+            public string DrinkAndDrive()
+            {
+                return "driving while drunk";
+            }
+        }
+
+        public class ResponsiblePerson
+        {
+            private Person person;
+            public ResponsiblePerson(Person person)
+            {
+                this.person = person;
+            }
+
+            public int Age
+            {
+                get
+                {
+                    return person.Age;
+                }
+                set
+                {
+                    person.Age = value;
+                }
+            }
+            public string Drink()
+            {
+                if (Age > 18)
+                    return person.Drink();
+
+                return "too young";
+
+            }
+
+            public string Drive()
+            {
+                if (Age > 16)
+                    return person.Drive();
+
+                return "too young";
+            }
+
+            public string DrinkAndDrive()
+            {
+                return "dead";
+            }
+        }
+    }
+
+}
