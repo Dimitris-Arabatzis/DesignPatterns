2141bb05bcc114731a9aa06c3918865ea8cf50eb
Dimitris Arabatzis <dimitris.arabatzis@cuberm.com>
2022-06-27 23:14:21 +0300

    Create a project to showcase chain of responsibility pattern.

---
+++ ChainOfResponsibility/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ChainOfResponsibility
+{
+
+    /// <summary>
+    /// A chain of components who all get a chance to process a command or a query,
+    /// optionally having default processing implementation and an ability to
+    /// terminate the processing chain.
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
