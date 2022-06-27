733720e3e4553c1f59a92f579a2eefb1037e02e8
Dimitris Arabatzis <dimitris.arabatzis@cuberm.com>
2022-06-27 23:48:37 +0300

    Implemented method chain

---
+++ ChainOfResponsibility/MethodChain.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainOfResponsibility
+{
+    internal class Creature
+    {
+        public string Name;
+        public int Attack, Defense;
+
+        public Creature(string name, int attack, int defense)
+        {
+            Name = name;
+            Attack = attack;
+            Defense = defense;
+        }
+
+        public override string? ToString()
+        {
+            return $"{nameof(Name)}: {Name}, {nameof(Attack)}: {Attack}, {nameof(Defense)}: {Defense}";
+        }
+    }
+
+    internal class CreatureModifier
+    {
+        protected Creature creature;
+        protected CreatureModifier next; //linked list
+
+        public CreatureModifier(Creature creature)
+        {
+            this.creature = creature;
+        }
+
+        public void Add(CreatureModifier cm)
+        {
+            if (next != null) next.Add(cm);
+            else next = cm;
+        }
+        public virtual void Handle() => next?.Handle();
+    }
+    internal class DoubleAttackModifier : CreatureModifier
+    {
+        public DoubleAttackModifier(Creature creature) : base(creature)
+        {
+        }
+
+        public void Add(CreatureModifier cm)
+        {
+            if (next != null) next.Add(cm);
+            else next = cm;
+        }
+        public override void Handle() 
+        {
+            Console.WriteLine($"Doubling {creature.Name}'s attack");
+            creature.Attack *= 2;
+            base.Handle();
+        }
+    }
+
+    internal class IncreasedDefenseModifier : CreatureModifier
+    {
+
+        public IncreasedDefenseModifier(Creature creature) : base(creature)
+        {
+        }
+
+        public override void Handle()
+        {
+            Console.WriteLine($"Increasing {creature.Name}'s defence");
+            creature.Defense += 3;
+            base.Handle();
+        }
+    }
+
+    internal class NoBonusesModifier : CreatureModifier
+    {
+        public NoBonusesModifier(Creature creature) : base(creature)
+        {
+        }
+
+        public override void Handle()
+        {
+            //
+        }
+    }
+
+}
--- ChainOfResponsibility/Program.cs
+++ ChainOfResponsibility/Program.cs
@@ -10,7 +10,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //-------------------Method Chain-------------------
+            var goblin = new Creature("Goblin", 2, 2);
+            Console.WriteLine(goblin);
+
+            var root = new CreatureModifier(goblin);
+
+            Console.WriteLine("Let's double the goblin's attack");
+            root.Add(new DoubleAttackModifier(goblin));
+
+            Console.WriteLine("Let's stop goblin from getting any modifiers");
+            root.Add(new NoBonusesModifier(goblin));
+
+            Console.WriteLine("Let's double the goblin's defense");
+            root.Add(new IncreasedDefenseModifier(goblin));
+
+            root.Handle();
+            Console.WriteLine(goblin);
+
         }
     }
 }
