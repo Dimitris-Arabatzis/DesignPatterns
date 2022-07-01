5b55fe5605b135a38c50bb0bc9454d5c20e00117
Dimitris Arabatzis <dimitris.arabatzis@cuberm.com>
2022-07-01 12:10:57 +0300

    Implemented Command design pattern

---
+++ Command/Command.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Command
+{
+    internal class BankAccount
+    {
+        private int balance;
+        int overdraftLimit = -500;
+
+        internal void Deposit(int amount)
+        {
+            balance += amount;
+            WriteLine($"Deposited ${amount}, balance is now {balance}");
+        }
+
+        internal void Withdraw(int amount)
+        {
+            if(balance-amount >= overdraftLimit)
+            {
+                balance -= amount;
+                WriteLine($"Withdrew ${amount}, balance is now {balance}");
+            }
+        }
+
+        public override string? ToString()
+        {
+            return $"{nameof(balance)}: {balance}";
+        }
+    }
+
+    internal interface ICommand
+    {
+        void Call();
+    }
+
+    internal class BankAccountCommand : ICommand
+    {
+        private BankAccount account;
+
+        public enum Action
+        {
+            Deposit, Withdraw
+        }
+        private Action action;
+        private int amount;
+
+        public BankAccountCommand(BankAccount account, Action action, int amount)
+        {
+            this.account = account;
+            this.action = action;
+            this.amount = amount;
+        }
+
+        public void Call()
+        {
+            switch (action)
+            {
+                case Action.Deposit:
+                    account.Deposit(amount);
+                    break;
+                case Action.Withdraw:
+                    account.Withdraw(amount);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+}
--- Command/Program.cs
+++ Command/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace Command
+﻿
+using static System.Console;
+
+namespace Command
 {
     internal class Program
     {
@@ -9,7 +12,18 @@
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            
+            //-------------------Command-------------------
+            var ba = new BankAccount();
+            var commands = new List<BankAccountCommand>
+            {
+                new BankAccountCommand(ba,BankAccountCommand.Action.Deposit,100),
+                new BankAccountCommand(ba,BankAccountCommand.Action.Withdraw,50),
+            };
+            WriteLine(ba);
+
+            commands.ForEach(c => c.Call());
+
+            WriteLine(ba);
         }
     }
 }
