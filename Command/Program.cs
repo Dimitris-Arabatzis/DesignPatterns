ba8d030f18c787d40673c2dc11b4775fc80109c8
Dimitris Arabatzis <dimitris.arabatzis@cuberm.com>
2022-07-01 11:34:04 +0300

    Ccreate project to showcase the command design pattern.

---
+++ Command/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Command
+{
+    internal class Program
+    {
+        /// <summary>
+        /// An object which represents an instruction to perform a particular action.
+        /// Contains all the information necessary for the action to be taken.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
