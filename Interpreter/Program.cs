753638062f2249a1fb83a29dd7f17bdf4d4bda1b
Dimitris Arabatzis <dimitris.arabatzis@cuberm.com>
2022-07-03 20:57:30 +0300

    Implemented lexing functionality.

--- Interpreter/Program.cs
+++ Interpreter/Program.cs
@@ -1,4 +1,6 @@
 ﻿
+using Interpreter;
+using System.Text;
 using static System.Console;
 
 namespace Command
@@ -14,8 +16,50 @@ namespace Command
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            
-            
+            //-------------------Lexing-------------------
+            string input = "(13+4)-(12+1)";
+            var tokens = Lex(input);
+            WriteLine(String.Join("\t", tokens));
+        }
+
+        static List<Token> Lex(string input)
+        {
+            var result = new List<Token>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                switch (input[i])
+                {
+                    case '+':
+                        result.Add(new Token(Token.Type.Plus, "+"));
+                        break;
+                    case '-':
+                        result.Add(new Token(Token.Type.Minus, "-"));
+                        break;
+                    case '(':
+                        result.Add(new Token(Token.Type.Lparen, "("));
+                        break;
+                    case ')':
+                        result.Add(new Token(Token.Type.Rparen, ")"));
+                        break;
+                    default:
+                        var sb = new StringBuilder(input[i].ToString());
+                        for (int j = i+1; j < input.Length; ++j)
+                        {
+                            if (char.IsDigit(input[j]))
+                            {
+                                sb.Append(input[j]);
+                                ++i;
+                            }
+                            else
+                            {
+                                result.Add(new Token(Token.Type.Integer, sb.ToString()));
+                                break;
+                            }
+                        }
+                        break;
+                }
+            }
+            return result;
         }
     }
 }
---
+++ Interpreter/Token.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interpreter
+{
+    internal class Token
+    {
+        public enum Type
+        {
+            Integer, Plus, Minus, Lparen, Rparen
+        }
+
+        public Type MyType;
+        public string Text;
+
+        public Token(Type myType, string text)
+        {
+            MyType = myType;
+            Text = text;
+        }
+
+        public override string? ToString()
+        {
+            return $"`{Text}`";
+        }
+    }
+}
