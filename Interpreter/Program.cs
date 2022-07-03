26d0fddeb0769a75ffc7103190feeae108a99a53
Dimitris Arabatzis <dimitris.arabatzis@cuberm.com>
2022-07-03 20:43:47 +0300

    Created solution for the interpreter design pattern.

---
+++ Interpreter/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using static System.Console;
+
+namespace Command
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Turning strings into OOP based structures in a complicated process.
+        /// A component that processes structured text data.
+        /// Does so by turning it into seperate lexical tokens (lexing)
+        /// and then interpreting sequences of said tokens (parsing).
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            
+            
+        }
+    }
+}
