08f51ed3f72edcea88a4ddd1ebe1b8446ee7e928
Dimitris Arabatzis <dimitris.arabatzis@cuberm.com>
2022-07-17 01:27:01 +0300

    Created solution for iterator design pattern

---
+++ Iterator/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Iterator
+{
+    internal class Program
+    {
+        /// <summary>
+        /// An object (or in .Net a method) that facilitates the traversal of a data structure.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            //------------------- -------------------
+
+
+        }
+    }
+}
