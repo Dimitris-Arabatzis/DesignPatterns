27979d03232caced7db6cfa09d226f37ec2fd0e3
Dimitris Arabatzis <dimitris.arabatzis@cuberm.com>
2022-07-19 21:17:55 +0300

    Implemented NullObject

---
+++ NullObject/NullObject.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NullObject
+{
+    public interface ILog
+    {
+        void Info(string msg);
+        void Warn(string msg);
+    }
+
+    public class NullLog : ILog
+    {
+        public void Info(string msg)
+        {
+            
+        }
+
+        public void Warn(string msg)
+        {
+            
+        }
+    }
+
+
+    public class ConsoleLog : ILog
+    {
+        public void Info(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+
+        public void Warn(string msg)
+        {
+            Console.WriteLine($"WARNING! {msg}");
+        }
+    }
+
+    internal class BankAccount 
+    {
+        private ILog log;
+        private int balance;
+
+        public BankAccount(ILog log)
+        {
+            this.log = log;
+        }
+
+        public void Deposit(int amount)
+        {
+            balance += amount;
+            log.Info($"Deposited {amount}. Balance = {balance}");
+        }
+    }
+}
--- NullObject/Program.cs
+++ NullObject/Program.cs
@@ -4,9 +4,18 @@ namespace NullObject
 {
     internal class Program
     {
+        /// <summary>
+        /// A no-op object that conforms to the required interface,
+        /// satisfying a dependency requirement of some other object.
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //------------------- Null Object Example -------------------
+
+            var log = new NullLog();
+            var ba = new BankAccount(log);
+            ba.Deposit(100);
         }
     }
 }
