79e5c00da3619ff993c3d914b57168a3ab97442d
Dimitris Arabatzis <dimitris.arabatzis@cuberm.com>
2022-07-25 20:24:42 +0300

    Created project to showcase the mediator design pattern

---
+++ Mediator/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Mediator
+{
+
+
+    /// <summary>
+    /// A component that facilitates communication  between other components
+    /// without them necessarily being aware of each other or having direct
+    /// (reference) access to each other,
+    /// Real world analogy is a Control tower for airplanes.
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //------------------- Mediator -------------------
+
+        }
+    }
+}
