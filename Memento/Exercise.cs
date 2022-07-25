f4ca8299f05eda9bd0a146c6251791e980a90433
Dimitris Arabatzis <dimitris.arabatzis@cuberm.com>
2022-07-25 20:11:57 +0300

    Added Exercise for memento.

---
+++ Memento/Exercise.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+
+namespace Coding.Exercise
+{
+    public class Token
+    {
+        public int Value = 0;
+
+        public Token(int value)
+        {
+            Value = value;
+        }
+    }
+
+    public class Memento
+    {
+        public List<Token> Tokens = new List<Token>();
+    }
+
+    public class TokenMachine
+    {
+        public List<Token> Tokens = new List<Token>();
+
+        public Memento AddToken(int value)
+        {
+            return AddToken(new Token(value));
+        }
+
+        public Memento AddToken(Token token)
+        {
+            Tokens.Add(token);
+            var m = new Memento();
+            m.Tokens = Tokens.Select(t => new Token(t.Value)).ToList();
+            return m;
+        }
+
+        public void Revert(Memento m)
+        {
+            Tokens = m.Tokens.Select(mm => new Token(mm.Value)).ToList();
+        }
+    }
+}
