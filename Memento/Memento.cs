ce358a965894321a7c22f918521a80e48384e73c
Dimitris Arabatzis <dimitris.arabatzis@cuberm.com>
2022-07-25 19:54:00 +0300

    Implemented the memento design pattern

---
+++ Memento/Memento.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memento
+{
+
+    public class Memento
+    {
+        public int Balance { get; }
+
+        public Memento(int balance)
+        {
+            Balance = balance;
+        }
+    }
+
+    internal class BankAccount
+    {
+        private int balance;
+        private List<Memento> changes = new List<Memento>();
+        private int current;
+
+        public BankAccount(int balance)
+        {
+            this.balance = balance;
+            changes.Add(new Memento(balance));
+        }
+
+        public Memento Deposit(int amount)
+        {
+            balance += amount;
+            var m = new Memento(balance);
+            changes.Add(m);
+            ++current;
+            return m;
+        }
+
+        public Memento Restore(Memento m)
+        {
+            if(m != null)
+            {
+                balance = m.Balance;
+                changes.Add(m);
+                return m;
+            }
+            return null;
+        }
+
+        public Memento Redo()
+        {
+
+            if (current + 1 < changes.Count)
+            {
+                var m = changes[++current];
+                balance = m.Balance;
+                return m;
+            }
+            return null;
+        }
+        public Memento Undo()
+        {
+            if (current > 0)
+            {
+                var m = changes[--current];
+                balance = m.Balance;
+                return m;
+            }
+            return null;
+        }
+
+        public override string? ToString()
+        {
+            return $"{nameof(balance)}: {balance}";
+        }
+
+    }
+}
---
+++ Memento/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Memento
+{
+
+    /// <summary>
+    /// A token/handle representing the system state.
+    /// Lets us roll back to the state when the token was generated.
+    /// May or may not directly expose state information.
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //------------------- Undo Redo -------------------
+
+            var ba = new BankAccount(100);
+            var m1 = ba.Deposit(50); //150
+            var m2 = ba.Deposit(25); //175
+
+            Console.WriteLine(ba);
+
+            ba.Undo();
+            Console.WriteLine($"Undo 1: {ba}");
+
+            ba.Undo();
+            Console.WriteLine($"Undo 2: {ba}");
+
+            ba.Redo();
+            Console.WriteLine($"Redo 1: {ba}");
+
+            ba.Redo();
+            Console.WriteLine($"Redo 2: {ba}");
+
+            //------------------- Undo Redo -------------------
+
+
+        }
+    }
+}
