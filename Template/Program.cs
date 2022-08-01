f6e14fe3dda6dc10f571f3bcfd746a08ad0a0c48
Dimitris Arabatzis <dimitris.arabatzis@cuberm.com>
2022-08-01 14:56:04 +0300

    Created project to implement the template design pattern.

---
+++ Template/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Template
+{
+
+    /// <summary>
+    /// Allows us to define the 'skeleton' of the algorithm with concrete implementations
+    /// defined in subclasses.
+    /// Fairly similar to strategy but uses inheritance instead of composition.
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
