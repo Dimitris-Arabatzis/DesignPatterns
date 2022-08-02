73d33eb90f5e75bb66361199004c7cc51c29f0eb
Dimitris Arabatzis <dimitris.arabatzis@cuberm.com>
2022-08-02 19:17:44 +0300

    Created project to showcase strategy pattern

---
+++ Strategy/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Strategy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //--------------------- Strategy ---------------------
+
+        }
+    }
+}
